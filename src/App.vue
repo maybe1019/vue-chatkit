c9ac9faf8359073ce327ebae15e252fb52ae120a
unknown <maybex66@gmail.com>
2022-11-11 11:03:29 -0800

    chat works

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,22 @@
       </v-btn>
 
       <v-btn @click="changeTheme">
-        <v-icon v-bind:icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"></v-icon>
+        <v-icon
+          v-bind:icon="
+            theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+          "
+        ></v-icon>
       </v-btn>
     </v-app-bar>
 
-
     <v-navigation-drawer v-if="loggedIn" floating permanent>
       <v-list density="compact" nav>
-        <v-list-item v-for="group in groups" :key="group.guid" :value="group.guid" @click="onGroupSelected(group)">
+        <v-list-item
+          v-for="group in groups"
+          :key="group.guid"
+          :value="group.guid"
+          @click="onGroupSelected(group)"
+        >
           <v-avatar :image="group.icon"></v-avatar>
           {{ group.name }}
         </v-list-item>
@@ -31,15 +39,40 @@
     </v-navigation-drawer>
 
     <v-main v-if="loggedIn">
+      <div v-for="msg in groupMessages" :key="msg.sentAt" class="px-3 py-1">
+        <div v-if="msg.category === 'message'" class="d-flex">
+          <v-avatar :image="msg.sender.avatar"></v-avatar>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ msg.sender.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ msg.data.text }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+        <div v-if="msg.category === 'action' && msg.action === 'added'" class="d-flex subtitle-2 px-5 text-blue">
+          <v-icon icon="mdi-arrow-right-bold"></v-icon> 
+          {{ msg.actionOn.name }} added
+        </div>
+      </div>
       <div class="message-input-group">
-        <v-text-field variant="outlined" label="" hide-details="true" class="pa-0" v-model="message"></v-text-field>
+        <v-text-field
+          variant="outlined"
+          label=""
+          hide-details="true"
+          class="pa-0"
+          v-model="myMessage"
+        ></v-text-field>
         <v-btn color="primary" @click="sendMessage">Send</v-btn>
       </div>
     </v-main>
 
     <v-main v-else class="d-flex flex-column align-center justify-center">
       <v-card width="350px" class="pa-3">
-        <v-text-field variant="solo" placeholder="Username" v-model="UID"></v-text-field>
+        <v-text-field
+          variant="solo"
+          placeholder="Username"
+          v-model="userId"
+        ></v-text-field>
         <v-btn width="100%" color="primary" @click="logIn">Log In</v-btn>
       </v-card>
     </v-main>
@@ -47,22 +80,23 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch } from "vue";
 import { CometChat } from "@cometchat-pro/chat";
 
-const theme = ref('dark')
-const myAccount = ref({})
-const loggedIn = ref(false)
-const UID = ref("superhero1")
-const groups = ref([])
-const message = ref('')
-let selectedGroup = {}
+const theme = ref("dark");
+const myAccount = ref({});
+const loggedIn = ref(false);
+const userId = ref("superhero1");
+const groups = ref([]);
+const myMessage = ref("");
+const groupMessages = ref([]);
+let selectedGroup = {};
 
 function changeTheme() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
+  theme.value = theme.value === "light" ? "dark" : "light";
 }
 
-const authKey = '5791fc9b99207a425427e4b193e0a2618804f410'; //eslint-disable-line
+const authKey = "5791fc9b99207a425427e4b193e0a2618804f410";
 
 const appID = "2250625da760e0df";
 const region = "us";
@@ -75,7 +109,7 @@ CometChat.init(appID, appSetting).then(
   () => {
     console.log("Initialization completed successfully");
   },
-  error => {
+  (error) => {
     console.log("Initialization failed with error:", error);
   }
 );
@@ -88,75 +122,115 @@ watch(loggedIn, (newLoggedIn) => {
       .build();
 
     groupsRequest.fetchNext().then(
-      groupList => {
-        groups.value = groupList
+      (groupList) => {
+        groups.value = groupList;
         console.log("Groups list fetched successfully", groupList);
-      }, error => {
+      },
+      (error) => {
         console.log("Groups list fetching failed with error", error);
       }
     );
   }
-})
+});
 
 const logIn = () => {
-  CometChat.getLoggedinUser().then(user => {
-    if (!user) {
-      CometChat.login(UID.value, authKey).then(
-        user => {
-          console.log("Login Successful:", { user });
-          loggedIn.value = true
-          myAccount.value = user
-        }, error => {
-          console.log("Login failed with exception:", { error });
-        }
-      );
+  CometChat.login(userId.value, authKey).then(
+    (user) => {
+      console.log("Login Successful:", { user });
+      loggedIn.value = true;
+      myAccount.value = user;
+      registerMessageListener()
+    },
+    (error) => {
+      console.log("Login failed with exception:", { error });
     }
-    else {
-      loggedIn.value = true
-      myAccount.value = user
-      console.log('Loginned User', user)
-    }
-  }, error => {
-    console.log("Something went wrong", error);
-  });
-}
+  );
+};
 
 const logOut = () => {
   CometChat.logout().then(
     () => {
       console.log("Logout completed successfully");
-      myAccount.value = {}
-      loggedIn.value = false
-    }, error => {
+      myAccount.value = {};
+      loggedIn.value = false;
+    },
+    (error) => {
       console.log("Logout failed with exception:", { error });
     }
   );
-}
+};
 
 const onGroupSelected = (group) => {
-  selectedGroup = group
-}
+  if (selectedGroup.guid !== group.guid) {
+    selectedGroup = group;
+    let GUID = group.guid;
+    let limit = 30;
+    let messagesRequest = new CometChat.MessagesRequestBuilder()
+      .setGUID(GUID)
+      .setLimit(limit)
+      .build();
+
+    messagesRequest.fetchPrevious().then(
+      (messages) => {
+        console.log("Message list fetched:", messages);
+        groupMessages.value = messages.map(msg => msg);
+      },
+      (error) => {
+        console.log("Message fetching failed with error:", error);
+      }
+    );
+  }
+};
 
 const sendMessage = () => {
   let receiverID = selectedGroup.guid;
-  let messageText = message.value;
+  let messageText = myMessage.value;
   let receiverType = CometChat.RECEIVER_TYPE.GROUP;
-  let textMessage = new CometChat.TextMessage(receiverID, messageText, receiverType);
+  let textMessage = new CometChat.TextMessage(
+    receiverID,
+    messageText,
+    receiverType
+  );
 
   CometChat.sendMessage(textMessage).then(
-    message => {
+    (message) => {
       console.log("Message sent successfully:", message);
-      message.value = ''      
-    }, error => {
+      groupMessages.value.push(message)
+      myMessage.value = "";
+    },
+    (error) => {
       console.log("Message sending failed with error:", error);
     }
+  );
+};
+
+const registerMessageListener = () => {
+  let listenerID = myAccount.value.uid;
+  console.log(listenerID)
+  CometChat.addMessageListener(
+      listenerID,
+      new CometChat.MessageListener({
+          onTextMessageReceived: textMessage => {
+              console.log("Text message received successfully", textMessage);
+              if(textMessage.receiver.guid === selectedGroup.guid) {
+                groupMessages.value.push(textMessage)
+                console.log(groupMessages.value)
+              }
+          },
+          onMediaMessageReceived: mediaMessage => {
+              console.log("Media message received successfully", mediaMessage);
+          },
+          onCustomMessageReceived: customMessage => {
+              console.log("Custom message received successfully", customMessage);
+          }
+      })
   );
 }
 
 </script>
 
 <style>
-@import './assets/scss/style.scss';
+@import "./assets/scss/style.scss";
 
 .message-input-group {
   position: fixed;
@@ -180,5 +254,9 @@ const sendMessage = () => {
   min-height: auto !important;
   display: flex !important;
   align-items: center !important;
+}
+
+main {
+  padding-bottom: 76px !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,12 +9,16 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
   components,
   directives,
   icons: {
     defaultSet: 'mdi',
+    sets: {
+      mdi,
+    }
   },
 })
 
